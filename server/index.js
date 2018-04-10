48cbf946800b18d22d71a609c2fd37e5f4bf9457
David Engelstad <davidengelstad@gmail.com>
2018-04-10 15:35:39 -0700

    Server and router outline

--- server/index.js
+++ server/index.js
@@ -1,1 +1,21 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const router = require('./routes')
+const db = require('./db/index.js')
+const path = require('path');
 
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use('/api', router);
+app.use(express.static(path.resolve(__dirname, '../client/dist')));
+
+
+
+let PORT = 3000;
+
+app.listen(3000, function() {
+    console.log(`Server is listening on ${PORT}`);
+})
