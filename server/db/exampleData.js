c1119a8da6545d7d8df93d3f4d4a388469802976
Aaron Melendez <aaronmelendez37@gmail.com>
2018-04-10 15:51:05 -0700

    Add ids to relevant fields within dummy data

--- server/db/exampleData.js
+++ server/db/exampleData.js
@@ -2,23 +2,23 @@ const users = [
     {
       username: 'nick123',
       password: 'abcd123',
-      subscriptions: [],
-      posts: [],
-      comments: []     
+      subscriptions: [4, 8, 12],
+      posts: [1, 2],
+      comments: [1]     
     },
     {
       username: 'joseph123',
       password: 'password123',
-      subscriptions: [],
-      posts: [],
-      comments: []     
+      subscriptions: [8],
+      posts: [3],
+      comments: [2]     
     },
     {
       username: 'david123',
       password: 'wooo123',
       subscriptions: [],
       posts: [],
-      comments: []     
+      comments: [3]     
     },
 ];
 
@@ -30,7 +30,7 @@ const comments = [
     text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
     parent: {},
     children: [2, 3],
-    post: {}
+    post: 1
   },
   {
     id: 2,
@@ -39,7 +39,7 @@ const comments = [
     text: 'Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text.',
     parent: 1,
     children: [],
-    post: {}
+    post: 1
   },
   {
     id: 3,
@@ -48,36 +48,42 @@ const comments = [
     text: 'More Lorem Ipsum More Lorem Ipsum More Lorem Ipsum More Lorem Ipsum More Lorem Ipsum.',
     parent: 1,
     children: [],
-    post: {}
+    post: 1
   }
 ];
 
 const subReddits = [
   {
+    id: 12,
     name: 'Stray Cat Wanders Into Police Station'
   },
   {
+    id: 8,
     name: 'Mark Zuckerberg while being questioned by the senate'
   },
   {
+    id: 4,
     name: 'This meme isn’t fine as it is'
   },
 ];
 
 const posts = [
   {
+    id: 1,
     url: 'https://i.imgur.com/lyEmz0U.jpg',
     likes: 57,
     username: 'nick123',
     subReddit: 'cats'
   },
   {
+    id: 2,
     url: 'https://i.imgur.com/i44HiFN.jpg',
     likes: 117,
     username: 'joseph123',
     subReddit: 'tech'
   },
   {
+    id: 3,
     url: 'https://www.reddit.com/r/memes/comments/8ba611/this_meme_isnt_fine_as_it_is/',
     likes: 312,
     username: 'david123',
