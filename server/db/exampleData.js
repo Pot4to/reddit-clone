be0184896e959b12116ac11a78f3311068bec5c3
Aaron Melendez <aaronmelendez37@gmail.com>
2018-04-10 15:36:21 -0700

    Create example data

---
+++ server/db/exampleData.js
@@ -0,0 +1,86 @@
+const users = [
+    {
+      username: 'nick123',
+      password: 'abcd123',
+      subscriptions: [],
+      posts: [],
+      comments: []     
+    },
+    {
+      username: 'joseph123',
+      password: 'password123',
+      subscriptions: [],
+      posts: [],
+      comments: []     
+    },
+    {
+      username: 'david123',
+      password: 'wooo123',
+      subscriptions: [],
+      posts: [],
+      comments: []     
+    },
+];
+
+const comments = [
+  {
+    id: 1,
+    author: 'nick123',
+    likes: 27,
+    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    parent: {},
+    children: [2, 3],
+    post: {}
+  },
+  {
+    id: 2,
+    author: 'joseph123',
+    likes: 27,
+    text: 'Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text.',
+    parent: 1,
+    children: [],
+    post: {}
+  },
+  {
+    id: 3,
+    author: 'david123',
+    likes: 27,
+    text: 'More Lorem Ipsum More Lorem Ipsum More Lorem Ipsum More Lorem Ipsum More Lorem Ipsum.',
+    parent: 1,
+    children: [],
+    post: {}
+  }
+];
+
+const subReddits = [
+  {
+    name: 'Stray Cat Wanders Into Police Station'
+  },
+  {
+    name: 'Mark Zuckerberg while being questioned by the senate'
+  },
+  {
+    name: 'This meme isn’t fine as it is'
+  },
+];
+
+const posts = [
+  {
+    url: 'https://i.imgur.com/lyEmz0U.jpg',
+    likes: 57,
+    username: 'nick123',
+    subReddit: 'cats'
+  },
+  {
+    url: 'https://i.imgur.com/i44HiFN.jpg',
+    likes: 117,
+    username: 'joseph123',
+    subReddit: 'tech'
+  },
+  {
+    url: 'https://www.reddit.com/r/memes/comments/8ba611/this_meme_isnt_fine_as_it_is/',
+    likes: 312,
+    username: 'david123',
+    subReddit: 'memes'
+  }
+];
