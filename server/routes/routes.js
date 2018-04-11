e41bac3743c1459419f5083433fc4cbdcdc9232b
Aaron Melendez <aaronmelendez37@gmail.com>
2018-04-11 16:44:15 -0700

    Set up routes to handle votes on posts & post title clicks

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+// handle POST for Vote
+router.post('/post/vote', (req, res) => res.status(201).send('Server received POST'));
 
+// handle GET for Post
+router.get('/post', (req, res) => res.status(200).send('Server received GET'));
 
 module.exports = router;
