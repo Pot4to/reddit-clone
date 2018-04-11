95856420f6b6f43f70cd3b87766bd66fc4b97ffd
Nick Vrdoljak <admin@nicholasvrdoljak.com>
2018-04-11 16:50:30 -0700

    Revised syntax

--- server/index.js
+++ server/index.js
@@ -13,12 +13,22 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use('/api', router);
 app.use(express.static(path.resolve(__dirname, '../client/dist')));
 
-app.get('/api/comments/*', function(req, res) {
-    db.recursiveGetComments(req.url.slice(14), function(err, data) {
+
+
+
+
+
+app.get('/api/comments/*', (req, res) => {
+    db.recursiveGetComments(req.url.slice(14), (err, data) => {
         if (err) return res.status(404).send();
         res.status(200).send(data);
     })
-})
+});
+
+
+
+
+
 
 let PORT = 3000;
 
