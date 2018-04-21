65799003a31b0a506f73f267db786b672ab2aa90
Nick Vrdoljak <admin@nicholasvrdoljak.com>
2018-04-20 19:34:55 -0700

    heroku

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.get('/favicon.ico', (req, res) => {
 });
 
 var allowCrossDomain = function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'example.com');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
