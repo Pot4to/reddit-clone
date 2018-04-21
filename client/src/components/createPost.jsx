9084e95013f72aa11d6d78db3acccee822c9f075
Nick Vrdoljak <admin@nicholasvrdoljak.com>
2018-04-20 19:20:52 -0700

    heroku

--- client/src/components/createPost.jsx
+++ client/src/components/createPost.jsx
@@ -17,7 +17,7 @@ class CreatePost extends React.Component {
     
     handleSubmit(e) {
         e.preventDefault();
-        axios.post(`/api/${this.props.username}/${this.state.title}/${this.state.url}/${this.state.text}/${this.props.subreddit._id}`)
+        axios.post(`/create-post/${this.props.username}/${this.state.title}/${this.state.url}/${this.state.text}/${this.props.subreddit._id}`)
         .then((e) => {
             // change view prop in index.jsx state to home page view
             console.log('after the post call')
