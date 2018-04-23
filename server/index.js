d3b3873910eda73141cb83fd1be76ba0999b758c
Nick Vrdoljak <admin@nicholasvrdoljak.com>
2018-04-22 21:02:54 -0700

    heroku

--- server/index.js
+++ server/index.js
@@ -15,11 +15,12 @@ var LocalStrategy = require('passport-local').Strategy;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
+app.use((req, res, next) => {
+res.header("Access-Control-Allow-Origin", "*");
+res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE");
+});
+
 app.use('/api', router);
 
 app.get('/favicon.ico', (req, res) => {
