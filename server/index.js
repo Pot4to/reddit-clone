c1336d8d68e497b10485957cffc09bce99f28771
Nick Vrdoljak <admin@nicholasvrdoljak.com>
2018-04-23 12:19:01 -0700

    heroku

--- server/index.js
+++ server/index.js
@@ -26,6 +26,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE");
+    console.log('arriving in the server');
     next();
 });
 
